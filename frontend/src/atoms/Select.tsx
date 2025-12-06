b111683aca247c457808bb80be0887f9e8f34e41
horun-f <horun.fujimoto@original-life.co.jp>
2025-12-07 01:27:38 +0900

    レイアウト微調整

--- frontend/src/atoms/Select.tsx
+++ frontend/src/atoms/Select.tsx
@@ -1,25 +1,32 @@
-import { Form, type FormSelectProps } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
 import classNames from 'classnames';
+import type React from 'react';
 
 type SelectOption = {
   value: string | number;
   label: string;
 };
 
-type SelectProps = FormSelectProps & {
+// SelectPropsをForm.Selectのプロップ型そのものに拡張し、optionsを追加
+type SelectProps = React.ComponentProps<typeof Form.Select> & {
   options: SelectOption[];
 };
 
 /**
- * Common Select/Dropdown Component
- * @param {SelectProps} props - The props for the component.
- * @returns {JSX.Element}
+ * Common Select/Dropdown Component, a wrapper around React-Bootstrap's Form.Select.
+ * This component is polymorphic and can be rendered as different elements
+ * using the `as` prop.
  */
-export const Select = ({ className, options, ...props }: SelectProps) => {
-  const selectClasses = classNames(className);
-
+export const Select = ({
+  className,
+  options,
+  ...props // as, isInvalid など全てのプロップがここに
+}: SelectProps) => {
   return (
-    <Form.Select className={selectClasses} {...props}>
+    <Form.Select
+      className={classNames(className)}
+      {...props}
+    >
       {options.map((option) => (
         <option key={option.value} value={option.value}>
           {option.label}
